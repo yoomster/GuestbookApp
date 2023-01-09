dec676237c3b61edc705227e999136f994fce252
naomifleurnoel <nperenboom@hotmail.com>
2023-01-09 11:32:17 +0100

    Add project files.

---
+++ GuestBook/ConsoleMethods.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuestBook
+{
+    public static class ConsoleMethods
+    {
+        public static string GroupName()
+        {
+            Console.Write("Enter the group name please: ");
+            string name = Console.ReadLine();
+
+            return name;
+        }
+
+        public static double GroupSize()
+        {
+            Console.Write("What is the size of your group: ");
+            string amountOfPeople = Console.ReadLine();
+            bool validInt = double.TryParse(amountOfPeople, out double num);
+
+            return num;
+        }
+
+        public static void AllFamilieNames(List<string> AllGuests)
+        {
+            foreach (var guest in AllGuests)
+            {
+            Console.WriteLine($"The group '{guest}' is present.");
+            }
+        }
+
+        public static void FamilySizeList(List<double> familySize)
+        {
+            Console.WriteLine($"Total people here is: {familySize.Sum()}");
+        }
+    }
+}
---
+++ GuestBook/Program.cs
@@ -0,0 +1,25 @@
+﻿using GuestBook;
+using System.Net;
+
+
+List<string> allGuests = new List<string>();
+List<double> totalGuests = new List<double>();
+
+string names;
+double peopleInGroup;
+
+do
+{
+	names = ConsoleMethods.GroupName();
+	if (names.ToLower() != "exit")
+{
+	allGuests.Add(names);
+	
+	peopleInGroup = ConsoleMethods.GroupSize();
+	totalGuests.Add(peopleInGroup);
+
+}
+} while (names.ToLower() != "exit");
+
+ConsoleMethods.AllFamilieNames(allGuests);
+ConsoleMethods.FamilySizeList(totalGuests);
